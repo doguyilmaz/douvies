6fd2efffee1860bca712235a3a2391f6b2145231
Kaan Yilmaz <douscriptist@gmail.com>
2021-10-24 02:53:26 +0300

    gitignore & axios url

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,31 +1,31 @@
 import axios from 'axios';
 // import { API_URL } from '../config';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'http://localhost:8080';
 
 let instance = axios.create({
-	baseURL: API_URL,
-	timeout: 4000,
-	timeoutErrorMessage: 'Request has timed out. Try again later.',
+  baseURL: API_URL,
+  timeout: 4000,
+  timeoutErrorMessage: 'Request has timed out. Try again later.',
 });
 
 // request header
 instance.interceptors.request.use(
-	(config) => {
-		// Do something before request is sent
+  (config) => {
+    // Do something before request is sent
 
-		// api token
-		// const apiToken = sessionStorage.getItem('token')
-		// config.headers = { 'Custom-Header-IF-Exist': apiToken }
-		// const apiToken = JSON.parse(localStorage.getItem('token'));
-		// alert(apiToken.token)
-		// config['Content-Type'] = 'application/json';
-		// config['headers'] = { Authorization: apiToken };
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
+    // api token
+    // const apiToken = sessionStorage.getItem('token')
+    // config.headers = { 'Custom-Header-IF-Exist': apiToken }
+    // const apiToken = JSON.parse(localStorage.getItem('token'));
+    // alert(apiToken.token)
+    // config['Content-Type'] = 'application/json';
+    // config['headers'] = { Authorization: apiToken };
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export const http = instance;
